ece4d281fdeea7a86c6ba4b344cff42dac1b8e78
DiditAdiyansah <147999327+DiditAdiyansah@users.noreply.github.com>
2023-11-04 09:05:40 +0700

    Add files via upload

---
+++ didit2.py
@@ -0,0 +1,7 @@
+angka2 = [2,3,4,5,5]
+print(angka2)
+
+for i in angka2:
+    print(f"i sekarang > {i}")
+
+print("akhir dari program")
---
+++ didit4.py
@@ -0,0 +1,4 @@
+ulang = 8
+
+for i in range(ulang) :
+    print("f Loop ke -{i}" "8")
